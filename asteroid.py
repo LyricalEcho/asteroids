c64c38f532ada3ebd6813206a7fa6211bdadb182
LyricalEcho <thomas.prater.5@gmail.com>
2025-04-24 22:22:15 -0500

    game works. no collisions yet.

---
+++ asteroid.py
@@ -0,0 +1,20 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.x = x
+        self.y = y
+        self.radius = radius
+    
+    def draw(self, screen):
+        pygame.draw.circle(
+            surface = screen,
+            color = (255,255,255),
+            center = self.position,
+            radius = self.radius,
+            width = 2)
+        
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import pygame
 from constants import *
-
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    
+    # Window/Screen display size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # FPS stuff
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    # Instantiate player
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    # Asteroid Field object
+    asteroid_field = AsteroidField()
+
+    # Game Loop
     while True:
         
         # Makes the window 'close' button work
@@ -17,6 +41,18 @@ def main():
         
         # Game window display
         pygame.Surface.fill(screen, (0,0,0))
+
+        # Update and Draw groups of player and objects on screen
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
+        # FPS
+        # Pause game loop until 1/60th of a second has passed
+        # and turn miliseconds into seconds
+        dt = clock.tick(60) / 1000
+
+        # Refresh the screen
         pygame.display.flip()
 
 
